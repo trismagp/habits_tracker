9278398a9f573e321c850b316cb5cec4659a411b
Tris <tris.magp@gmail.com>
2025-03-02 14:19:20 +0100

    first commit

---
+++ android/app/src/main/kotlin/com/example/habits_tracker/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.habits_tracker
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const HabitTrackerApp());
+}
+
+class HabitTrackerApp extends StatelessWidget {
+  const HabitTrackerApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Habit Tracker',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('My Habits'),
+      ),
+      body: const Center(
+        child: Text('No habits yet!'),
+      ),
+    );
+  }
+}
